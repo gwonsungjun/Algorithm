68f90fd1b2cdd6ab95f8b63cc42f424e64899bb8
Sungjun <sungjunpizz@gmail.com>
2018-09-11 22:07:31 +0900

    Update Problem
    MaxCounters

--- src/main/java/codility/MaxCounters.java
+++ src/main/java/codility/MaxCounters.java
@@ -7,31 +7,20 @@ public class MaxCounters {
     public static int[] solution(int N, int[] A) {
 
         int length = A.length;
-        int[] counter = new int[N];
-        Arrays.fill(counter, 0);
+        int[] counters = new int[N];
+        int max = 0;
 
         for (int i = 0; i < length; i++) {
             int val = A[i];
             if (1 <= val && val <= N) {
-                counter[val - 1] += 1;
-            } else if (val == N + 1) {
-                int num = getMaxVal(counter);
-                Arrays.fill(counter, num);
+                counters[val - 1] += 1;
+                max = Math.max(max, counters[val - 1]);
+            } else if (val == N + 1)  {
+                Arrays.fill(counters, max);
             }
         }
 
-        return counter;
-    }
-
-    public static int getMaxVal(int[] counter) {
-        int length = counter.length;
-        int max = 0;
-        for (int i = 0; i < length; i++) {
-            if (counter[i] > max) {
-                max = counter[i];
-            }
-        }
-        return max;
+        return counters;
     }
 
     public static void main(String[] args) {
@@ -42,3 +31,35 @@ public class MaxCounters {
         Arrays.stream(result).forEach(System.out::print);
     }
 }
+
+
+
+/*
+
+    int length = A.length;
+    int[] counter = new int[N];
+        Arrays.fill(counter, 0);
+
+                for (int i = 0; i < length; i++) {
+        int val = A[i];
+        if (1 <= val && val <= N) {
+        counter[val - 1] += 1;
+        } else if (val == N + 1) {
+        int num = getMaxVal(counter);
+        Arrays.fill(counter, num);
+        }
+        }
+
+        return counter;
+        }
+
+public static int getMaxVal(int[] counter) {
+        int length = counter.length;
+        int max = 0;
+        for (int i = 0; i < length; i++) {
+        if (counter[i] > max) {
+        max = counter[i];
+        }
+        }
+        return max;
+*/
