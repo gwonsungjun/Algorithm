2c54e67d3568d3c361cebf2d49997ac490a30b3d
Sungjun <sungjunpizz@gmail.com>
2018-09-11 10:19:48 +0900

    Update  2 Problems
    PermCheck, TapeEquilibrium

--- src/main/java/codility/PermCheck.java
+++ src/main/java/codility/PermCheck.java
@@ -1,29 +1,62 @@
 package codility;
 
+//import java.util.Arrays;
+//import java.util.HashSet;
+//import java.util.Set;
+
 import java.util.Arrays;
+
+import java.util.stream.IntStream;
 
 public class PermCheck {
 
     public static int solution(int[] A) {
-        int length = A.length;
+        int max = IntStream.of(A).max().orElse(0);
+        int length = (int)Arrays.stream(A).distinct().count();
 
-        Arrays.sort(A);
-
-        for (int i = 0; i < length-1; i++) {
-            if (A[i + 1] != A[i] + 1) {
-                return 0;
-            }
+        if(max==length && length == A.length){
+            return 1;
+        }else{
+            return 0;
         }
 
-        return 1;
     }
 
     public static void main(String[] args) {
-        //int[] A = {4, 1, 3, 2};
+        int[] A = {4, 1, 3, 2};
         //int[] A = {1, 1};
         //int[] A = {4, 1, 3};
-        //int[] A = {-1, -2};
-        int[] A = {1};
+        //int[] A = {1};
+        //int[] A = {1, 4, 1};
+
         System.out.println(solution(A));
     }
 }
+
+     /*   Arrays.sort(A);
+                for (int i = 0; i < length-1; i++) {
+        if (A[i + 1] != A[i] + 1) {
+        return 0;
+        }
+        }
+
+        return 1;*/
+
+
+     /*
+        int total = 0;
+        int arraySum = 0;
+
+        for(int i=1; i <= length; i++){
+            total += i;
+        }
+
+        for(int i=0; i < length; i++){
+            arraySum += A[i];
+        }
+
+        if(total-arraySum ==0){
+            return 1;
+        }else{
+            return 0;
+        }*/
--- src/main/java/codility/TapeEquilibrium.java
+++ src/main/java/codility/TapeEquilibrium.java
@@ -1,33 +1,60 @@
 package codility;
 
-import java.util.Arrays;
-
 public class TapeEquilibrium {
 
     public static int solution(int A[]) {
+
         int N = A.length;
-        int result = 0;
-        int sumPrev = 0;
+
+        int sumTotal = 0;
+        int sumLeft = 0;
+        int sumRight = 0;
+        int result = Integer.MAX_VALUE;
+
         for (int i = 0; i < N; i++) {
-            sumPrev += A[i];
-            int sumNext = 0;
-            for (int j = i+1; j < N; j++){
-                sumNext += A[j];
-            }
-            int abs = Math.abs(sumNext-sumPrev);
-            if (i==0){
-                result = abs;
-                continue;
-            }
-            if(abs < result){
-                result = abs;
-            }
+            sumTotal += A[i];
         }
+
+        for (int i = 0; i < N-1; i++) {
+            sumLeft += A[i];
+            sumRight = sumTotal - sumLeft;
+            result = Math.min(result, Math.abs(sumLeft-sumRight));
+        }
+
         return result;
     }
 
     public static void main(String[] args) {
-        int[] A = {3, 1, 2, 4, 3};
+        int[] A = {-10, -20, -30, -40, 100};
+        //int[] A = {3, 1, 2, 4, 3};
         System.out.print(solution(A));
     }
 }
+
+/*
+    public static int solution(int A[]) {
+        int N = A.length;
+
+        if (N == 2) {
+            return Math.abs(A[0] - A[1]);
+        }
+
+        int result = 0;
+        int sumPrev = 0;
+
+        for (int i = 0; i < N-1; i++) {
+            sumPrev += A[i];
+            int sumNext = 0;
+            for (int j = i + 1; j < N; j++) {
+                sumNext += A[j];
+            }
+            int abs = Math.abs(sumPrev - sumNext);
+            if (i == 0) {
+                result = abs;
+            }
+            if (abs < result) {
+                result = abs;
+            }
+        }
+        return result;
+    }*/
